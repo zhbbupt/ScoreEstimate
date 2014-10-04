cab426588ce3cd96c08a127898df320931a7bd6d
zhbbupt <913950506@qq.com>
2014-10-04 11:34:24 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,5 +3,9 @@ Created on 2014-10-2
 
 @author: zhanghb
 '''
+import datetime   
+starttime = datetime.datetime.now()
 import ScoreEstimate
 ScoreEstimate.Estimate('data/recommendation.txt','data/result.txt')
+endtime=datetime.datetime.now()
+print "runtime=",endtime-starttime
